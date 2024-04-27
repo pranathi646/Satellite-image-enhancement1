8c034172fb8831306fa18905d73effbbe8c1d70c
Pranathi Vannemreddy <144059653+pranathi646@users.noreply.github.com>
2024-04-27 13:22:01 +0530

    Create edge-detection.py

---
+++ edgedetection.py
@@ -0,0 +1,9 @@
+#edge detection (segmentation)
+assert img is not None, "file could not be read, check with
+os.path.exists()"
+edges = cv2.Canny(img,100,200)
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
